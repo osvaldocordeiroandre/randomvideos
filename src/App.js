93eeee4697df023813c97f0c8bf927f926be5d42
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2023-03-23 17:30:33 -0300

    deploy random videos web app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
+
+  const VideosUrl = [
+
+    'https://www.youtube.com/watch?v=tcA66TUoxqs',
+    'https://www.youtube.com/watch?v=lvj3uioKVjk',
+    'https://www.youtube.com/watch?v=HoTbtvF1iSw',
+    'https://youtu.be/QrZQLuJj2TE?t=380',
+    'https://www.youtube.com/watch?v=ettaeKZHAwA',
+    'https://youtu.be/yhtLGnExKYk?t=2752',
+    'https://www.youtube.com/watch?v=bQ-E2pWC-cE',
+    'https://www.youtube.com/watch?v=to8yh83jlXg',
+    'https://www.youtube.com/watch?v=a-GqSWsISVs',
+    'https://www.youtube.com/watch?v=wcG8jgU4-8A',
+    'https://www.youtube.com/watch?v=v3jti86s8RM',
+    'https://youtu.be/LPULcrW1gc0?t=6',
+    'https://www.youtube.com/watch?v=FMj4kj3oSc0',
+    'https://www.youtube.com/watch?v=gho9DNAY52g',
+    'https://www.youtube.com/watch?v=b393rRjMIm8',
+    'https://www.youtube.com/watch?v=ZZ5LpwO-An4',
+    'https://www.youtube.com/watch?v=wNkSIhRKAS4',
+    'https://www.youtube.com/watch?v=TMO4NH8HAHQ',
+    'https://www.youtube.com/watch?v=9xEvh1ForDE',
+    'https://www.youtube.com/watch?v=BBSqiqqt_7E',
+    'https://www.youtube.com/watch?v=XfqOB4hvxlY',
+    'https://www.youtube.com/watch?v=SrT9PiCzEC4',
+    'https://www.youtube.com/watch?v=oyY1ifuyUD0',
+    'https://www.youtube.com/watch?v=7BqLKGJZ9lI',
+    'https://www.youtube.com/watch?v=XWP24qoWxyM',
+    'https://www.youtube.com/watch?v=1tpIxo4vy80',
+    'https://www.youtube.com/watch?v=CzwVoc1MsGo',
+    'https://www.youtube.com/watch?v=NoMTPeRPVgw',
+    'https://www.youtube.com/watch?v=r5_NvFTfAWc',
+    'https://www.youtube.com/watch?v=fogmhYmSI-8',
+    'https://www.youtube.com/watch?v=o1gRV9zjAYQ',
+    'https://www.youtube.com/watch?v=WPlrfmxYmNc',
+    'https://youtu.be/0PiqqbyQRoo?t=16',
+    'https://www.youtube.com/watch?v=CLbidRKGyb0',
+    'https://www.youtube.com/watch?v=THgySrS02wM',
+    'https://www.youtube.com/watch?v=lrTeLurUybQ',
+    'https://www.youtube.com/watch?v=69xc-OMHt84',
+    'https://www.youtube.com/watch?v=bwskO02Bj7k'
+
+  ];
+
+  const handleClick = () => {
+
+    const randomVideosIndex = Math.floor(Math.random() * VideosUrl.length);
+    const randomVideosUrl = VideosUrl[randomVideosIndex];
+    window.open(randomVideosUrl, '_blank');
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='container'>
 
-export default App;
+      <button onClick={handleClick}> Vamos ver o que virá 👀</button>
+
+    </div>
+  )
+}
